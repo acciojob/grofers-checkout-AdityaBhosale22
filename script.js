906ef2ff87c0ba7d68443a36d639639ac1a16236
Aditya Bhosale <147322292+AdityaBhosale22@users.noreply.github.com>
2026-01-21 23:09:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
+getSumBtn.innerText = "Get Total Price";
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const prices = document.querySelectorAll(".price");
+
+  let total = 0;
+
+  prices.forEach(price => {
+    total += Number(price.textContent);
+  });
+
+  const table = document.querySelector("table");
+  const tr = document.createElement("tr");
+  const td = document.createElement("td");
+
+  td.colSpan = 2;
+  td.innerText = `Total Price = Rs ${total}`;
+
+  tr.appendChild(td);
+  table.appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
